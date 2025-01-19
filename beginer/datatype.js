b4a53381c924a33c9677df7231aeb5a340ab5a55
Arjun <alotcom837@gmail.com>
2025-01-19 16:00:33 +0000

    datatypes basics

---
+++ beginer/datatype.js
@@ -0,0 +1,14 @@
+// primitive data types
+// 7 types: number, bollean, string, symbol,null undefine
+
+let namee = "arjun"
+let emailecheck = true
+let age = 18
+let progress = null
+let service;
+
+console.table([typeof namee, typeof emailecheck,typeof age,typeof progress,typeof service]);
+// reference (non primitive)
+// type: array ,object , function
+
+
