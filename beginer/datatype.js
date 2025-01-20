d6ce47c5f61b09d41707f141933874155e1d5821
Arjun <alotcom837@gmail.com>
2025-01-20 16:12:12 +0000

    dataypes checked

--- beginer/datatype.js
+++ beginer/datatype.js
@@ -1,14 +1,40 @@
 // primitive data types
-// 7 types: number, bollean, string, symbol,null undefine
+// 7 types: number, bollean, string, symbol,bigint,null ,undefine
 
 let namee = "arjun"
 let emailecheck = true
 let age = 18
+let telno =97844093024248n
 let progress = null
-let service;
+let service =undefined
+const id=Symbol('421')
+const anotherid=Symbol('421')
+console.log(id === anotherid)
 
-console.table([typeof namee, typeof emailecheck,typeof age,typeof progress,typeof service]);
+console.table([typeof namee, typeof emailecheck,typeof age,typeof progress,typeof service,typeof anotherid,typeof telno]);
 // reference (non primitive)
 // type: array ,object , function
+// array
+const heros = ["rajeshamal","Nikhil upreti"]
+console.log(heros)
+// object
+let obj = {
+    name :"arjun",
+    age:18,
+
+}
+
+// function
+
+let myfunction = function(){
+    console.log("hello world")
+
+}
+console.table([typeof myfunction,typeof obj,typeof heros])
 
 
+let check =[ 
+{value:namee, type:typeof namee},
+{value:age,type:typeof age}
+];
+console.table(check)
