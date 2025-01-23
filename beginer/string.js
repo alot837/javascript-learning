c5989b9dcbe8bccfd0cb0d07d5e9173ff8c812f0
Arjun <alotcom837@gmail.com>
2025-01-23 17:34:09 +0000

    sting functions like (.substring,.slice,.splite)

--- beginer/string.js
+++ beginer/string.js
@@ -11,9 +11,25 @@ console.log(`My name is ${name} and my number of repo is sthe ${repo} `); //(`)t
 // another way to decleare the string 
 // this will create a such envaronment from which many function like charat lowercase can be accessed
 
-const gameName = new String('sumbhai');
+const gameName = new String('sumbhai-873-done');
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('a'));
 
+
+// the use of such more functions of strings 
+// visit mdn for the detail research and knowledge
+
+
+// substring is used to divide the string as we want but here the cutting of string from back is not possible
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+// slice is also used to cut the string from desired place but we can also cut from the back side
+const check = new String("arjun the boss")
+const anotherString = check.slice(4)
+console.log(anotherString)
+
+// by using the split feature of string we can split the string into array 
+console.log(check.split(" "))
