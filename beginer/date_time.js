03fce8db1f62b2cc5fdc6d031d74888ac13a5828
Arjun <alotcom837@gmail.com>
2025-01-29 16:50:06 +0000

    date and time

---
+++ beginer/date_time.js
@@ -0,0 +1,12 @@
+// let mydate =new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(typeof mydate)
+
+//  let myCreatedDate = new Date(2024, 01,16)
+// console.log(myCreatedDate.toDateString())
+
+// for printing the current date and time
+let timestamp =  Date.now();
+console.log(Date(timestamp))
