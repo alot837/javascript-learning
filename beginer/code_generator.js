b3c5efb0784b081bef78114225b15c1eb70a0ac9
Arjun <alotcom837@gmail.com>
2025-01-29 16:13:29 +0000

    code generator-

--- beginer/code_generator.js
+++ beginer/code_generator.js
@@ -4,3 +4,4 @@ for(i=0;i<5;i++)
     min = 100000
 let code = Math.floor(Math.random()*(max - min)+min);
 console.log(code)}
+// the above mention max and mini value will decide how many digits code will be generated
