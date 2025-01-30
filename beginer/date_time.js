df651de836df54821d498c9e6e6857a08b2ed022
Arjun <alotcom837@gmail.com>
2025-01-30 14:47:14 +0000

    detailed  date and time

--- beginer/date_time.js
+++ beginer/date_time.js
@@ -4,9 +4,21 @@
 // console.log(mydate.toDateString())
 // console.log(typeof mydate)
 
-//  let myCreatedDate = new Date(2024, 01,16)
+ let myCreatedDate = new Date(2024, 01,16)
 // console.log(myCreatedDate.toDateString())
 
 // for printing the current date and time
-let timestamp =  Date.now();
-console.log(Date(timestamp))
+// let timestamp =  Date.now();
+// console.log(Date(timestamp))
+// console.log(myCreatedDate.toLocaleTimeString())
+
+// date
+let new_date = new Date()
+// console.log(new_date);
+
+// console.log(new_date.getMonth()+1)
+console.log(`${new_date.toLocaleString('default',{
+    weekday: "long"
+})} is the todays date`)
+// console.log(new_date.toLocaleString('default',{
+    // weekday: "long", }))
